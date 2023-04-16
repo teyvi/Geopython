8a3e214bb59f8104e692a972ce94fb85d19f0741
TheChild <44750503+teyvi@users.noreply.github.com>
2023-04-16 04:46:49 +0000

    Add files via upload
    print statements for specific month

---
+++ average_temps.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 16 04:37:28 2023
+
+@author: angelateyvi
+"""
+
+selectedMonth = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+temperature = [-3.5, -4.5, -1.0, 4.0, 10, 15, 18, 16, 11.5, 6.0, 2.0, -1.5]
+print('The average temperature in Helsinki in', selectedMonth[2], 'is', temperature[2])
